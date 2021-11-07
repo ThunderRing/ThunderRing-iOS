b095700e58a47dac746c7e939c41c95933889c39
so yeon <alicesysy@swu.ac.kr>
2021-11-07 21:19:15 +0900

    [chore] foldering structure 적용

---
+++ ThunderRing/ThunderRing/Global/Extensions/UIView+.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+.swift
+//  ThunderRing
+//
+//  Created by soyeon on 2021/11/07.
+//
+
+import Foundation
--- ThunderRing/ThunderRing/Sources/Main/MainVC.swift
+++ ThunderRing/ThunderRing/Sources/Main/MainVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
