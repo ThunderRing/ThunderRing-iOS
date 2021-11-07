ab3a658f6598df028c86b8d153772610e8da97b1
so yeon <alicesysy@swu.ac.kr>
2021-11-07 21:34:21 +0900

    [add] extension file 추가

--- ThunderRing/ThunderRing/Global/Extensions/UIView+.swift
+++ ThunderRing/ThunderRing/Global/Extensions/UIView+.swift
@@ -5,4 +5,10 @@
 //  Created by soyeon on 2021/11/07.
 //
 
-import Foundation
+import UIKit
+
+extension UIView {
+    func addSubviews(_ views: [UIView]) {
+        views.forEach { self.addSubview($0) }
+    }
+}
