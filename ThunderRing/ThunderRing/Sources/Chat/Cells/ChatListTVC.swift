585b740e8081727784fc01b55ae5ad1a7cdf4a40
so yeon <alicesysy@swu.ac.kr>
2021-11-16 01:03:26 +0900

    [fix] image file error 해결

--- ThunderRing/ThunderRing/Sources/Chat/Cells/ChatListTVC.swift
+++ ThunderRing/ThunderRing/Sources/Chat/Cells/ChatListTVC.swift
@@ -7,6 +7,9 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class ChatListTVC: UITableViewCell {
     static let identifier = "ChatListTVC"
 
@@ -16,34 +19,57 @@ class ChatListTVC: UITableViewCell {
         $0.backgroundColor = .white
         $0.layer.borderWidth = 1
         $0.layer.borderColor = UIColor.lightGray.cgColor
+        $0.layer.cornerRadius = 10
+        $0.layer.masksToBounds = true
+    }
+    
+    private var chatImageView = UIImageView().then {
+        $0.image = UIImage(named: "image1")
+        $0.layer.cornerRadius = $0.frame.width / 2
+        $0.layer.masksToBounds = true
+    }
+    
+    private var emptyView = UIView().then {
+        $0.frame = CGRect(x: 0, y: 0, width: 10, height: 12)
     }
     
     private var hashTagLabel = UILabel().then {
-        $0.text = " 양파링 걸즈 "
-        $0.textColor = .black
+        $0.text = "양파링 걸즈"
+        $0.textColor = .purple
         $0.font = .SpoqaHanSansNeo(type: .regular, size: 12)
-        $0.layer.borderWidth = 1
-        $0.layer.borderColor = UIColor.purple.cgColor
+//        $0.layer.borderWidth = 1
+//        $0.layer.borderColor = UIColor.purple.cgColor
+    }
+    
+    private var labelStackView = UIStackView().then {
+        $0.alignment = .fill
+        $0.axis = .horizontal
+        $0.distribution = .fillEqually
+        $0.spacing = 1
     }
     
     private var titleLabel = UILabel().then {
-        $0.text = "모각공 할 사람"
+        $0.text = "혜화역 혼가츠 먹어요!"
         $0.textColor = .black
+        $0.font = .SpoqaHanSansNeo(type: .bold, size: 16)
     }
     
     private var subTitleLabel = UILabel().then {
         $0.text = "채팅을 먼저 시작해보세요.."
         $0.textColor = .darkGray
+        $0.font = .SpoqaHanSansNeo(type: .regular, size: 14)
     }
     
     private var countLabel = UILabel().then {
-        $0.text = "0"
+        $0.text = "8"
         $0.textColor = .lightGray
+        $0.font = .SpoqaHanSansNeo(type: .medium, size: 16)
     }
     
     private var timeLabel = UILabel().then {
         $0.text = "시간 전"
         $0.textColor = .lightGray
+        $0.font = .SpoqaHanSansNeo(type: .regular, size: 12)
     }
     
     // MARK: - Properties
@@ -68,24 +94,36 @@ class ChatListTVC: UITableViewCell {
 
 }
 
+// MARK: - Custom Methods
+
 extension ChatListTVC {
-    func setLayout() {
+    private func setLayout() {
         contentView.addSubview(backView)
-        backView.addSubviews([hashTagLabel, titleLabel, subTitleLabel, countLabel, timeLabel])
+        backView.addSubviews([chatImageView, labelStackView, titleLabel, subTitleLabel, countLabel, timeLabel])
+        
+        labelStackView.addArrangedSubview(emptyView)
+        labelStackView.addArrangedSubview(hashTagLabel)
+        labelStackView.addArrangedSubview(emptyView)
         
         backView.snp.makeConstraints {
             $0.leading.trailing.equalToSuperview().inset(25)
             $0.top.bottom.equalToSuperview().inset(7)
         }
         
-        hashTagLabel.snp.makeConstraints {
+        chatImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16)
+            $0.centerY.equalToSuperview()
+            $0.width.height.equalTo(56)
+        }
+        
+        labelStackView.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(83)
             $0.top.equalToSuperview().inset(16)
         }
         
         titleLabel.snp.makeConstraints {
             $0.leading.equalToSuperview().inset(83)
-            $0.top.equalTo(hashTagLabel.snp.bottom).offset(5)
+            $0.top.equalTo(labelStackView.snp.bottom).offset(5)
         }
         
         subTitleLabel.snp.makeConstraints {
@@ -104,3 +142,4 @@ extension ChatListTVC {
         }
     }
 }
+
--- ThunderRing/ThunderRing/Sources/TabBarController.swift
+++ ThunderRing/ThunderRing/Sources/TabBarController.swift
@@ -22,24 +22,24 @@ class TabBarController: UITabBarController {
     private func setTabBar() {
         let mainStoryboard = UIStoryboard.init(name: Const.Storyboard.Name.Main, bundle: nil)
         let mainTab = mainStoryboard.instantiateViewController(identifier: Const.ViewController.Name.Navigation)
-        mainTab.tabBarItem = UITabBarItem(title: "메인", image: UIImage(named: ""), selectedImage: UIImage(named: ""))
+        mainTab.tabBarItem = UITabBarItem(title: "메인", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
         
         
         let chatStoryboard = UIStoryboard.init(name: Const.Storyboard.Name.Chat, bundle: nil)
         let chatTab = chatStoryboard.instantiateViewController(identifier: Const.ViewController.Name.Navigation)
-        chatTab.tabBarItem = UITabBarItem(title: "채팅", image: UIImage(named: ""), selectedImage: UIImage(named: ""))
+        chatTab.tabBarItem = UITabBarItem(title: "채팅", image: UIImage(systemName: "dialog"), selectedImage: UIImage(systemName: "dialog.fill"))
         
         let lightningStoryboard = UIStoryboard.init(name: Const.Storyboard.Name.Lightning, bundle: nil)
         let lightningTab = lightningStoryboard.instantiateViewController(identifier: Const.ViewController.Name.Lightning)
-        lightningTab.tabBarItem = UITabBarItem(title: "번개", image: UIImage(named: ""), selectedImage: UIImage(named: ""))
+        lightningTab.tabBarItem = UITabBarItem(title: "번개", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
         
         let alarmStoryboard = UIStoryboard.init(name: Const.Storyboard.Name.Alarm, bundle: nil)
         let alarmTab = alarmStoryboard.instantiateViewController(identifier: Const.ViewController.Name.Navigation)
-        alarmTab.tabBarItem = UITabBarItem(title: "알림", image: UIImage(named: ""), selectedImage: UIImage(named: ""))
+        alarmTab.tabBarItem = UITabBarItem(title: "알림", image: UIImage(systemName: "alarm"), selectedImage: UIImage(systemName: "alarm.fill"))
         
         let myStoryboard = UIStoryboard.init(name: Const.Storyboard.Name.MyPage, bundle: nil)
         let myTab = myStoryboard.instantiateViewController(identifier: Const.ViewController.Name.Navigation)
-        myTab.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(named: ""), selectedImage: UIImage(named: ""))
+        myTab.tabBarItem = UITabBarItem(title: "마이페이지", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
         
         let tabs =  [mainTab, chatTab, lightningTab, alarmTab, myTab]
         
