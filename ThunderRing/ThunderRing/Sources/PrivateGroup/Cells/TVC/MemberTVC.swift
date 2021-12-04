b6a64f816f643f534148ca336792ab53f4fb5f3d
so yeon <alicesysy@swu.ac.kr>
2021-12-04 15:15:02 +0900

    [feat] 비공개그룹 생성 UI 구현

---
+++ ThunderRing/ThunderRing/Sources/PrivateGroup/Cells/TVC/MemberTVC.swift
@@ -0,0 +1,92 @@
+//
+//  MemberTVC.swift
+//  ThunderRing
+//
+//  Created by soyeon on 2021/12/04.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class MemberTVC: UITableViewCell {
+    static let identifier = "MemberTVC"
+    
+    // MARK: - UI
+    
+    private var userImageView = UIImageView().then {
+        $0.image = UIImage(named: "image2")
+    }
+    
+    private var userNameLabel = UILabel().then {
+        $0.text = "이름"
+        $0.textColor = .gray100
+        $0.font = .SpoqaHanSansNeo(type: .medium, size: 16)
+    }
+    
+    private var phoneNumberLabel = UILabel().then {
+        $0.text = "010-1234-5678"
+        $0.textColor = .gray100
+        $0.font = .DINPro(type: .regular, size: 14)
+    }
+    
+    private var checkButton = UIButton().then {
+        $0.setTitle("", for: .normal)
+        $0.setImage(UIImage(named: "btnCheckIn"), for: .normal)
+    }
+
+    // MARK: - Life Cycle
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        initUI()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        if selected {
+            checkButton.setImage(UIImage(named: "btnCheck"), for: .normal)
+        } else {
+            checkButton.setImage(UIImage(named: "btnCheckIn"), for: .normal)
+        }
+    }
+}
+
+extension MemberTVC {
+    private func initUI() {
+        self.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        addSubviews([userImageView, userNameLabel, phoneNumberLabel, checkButton])
+        
+        userImageView.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(20)
+            $0.centerY.equalToSuperview()
+            $0.width.equalTo(55)
+            $0.height.equalTo(48)
+        }
+        
+        userNameLabel.snp.makeConstraints {
+            $0.leading.equalTo(userImageView.snp.trailing).offset(15)
+            $0.top.equalToSuperview().inset(22)
+        }
+        
+        phoneNumberLabel.snp.makeConstraints {
+            $0.leading.equalTo(userImageView.snp.trailing).offset(15)
+            $0.top.equalTo(userNameLabel.snp.bottom).offset(1)
+        }
+        
+        checkButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(23)
+            $0.centerY.equalToSuperview()
+        }
+    }
+}
---
+++ ThunderRing/ThunderRing/Sources/PrivateGroup/VCs/CreatePrivateDetailVC.swift
@@ -0,0 +1,163 @@
+//
+//  CreatePrivateDetailVC.swift
+//  ThunderRing
+//
+//  Created by soyeon on 2021/12/02.
+//
+
+import UIKit
+
+class CreatePrivateDetailVC: UIViewController {
+
+    // MARK: - UI
+    
+    @IBOutlet weak var customNavigationBarView: UIView!
+    
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var descriptionCountLabel: UILabel!
+    
+    @IBOutlet weak var memberCountLabel: UILabel!
+    @IBOutlet weak var addMemberButton: UIButton!
+    @IBOutlet weak var memberCollectionView: UICollectionView!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
+    // MARK: - Properties
+    
+    var groupName = "그룹명"
+    var members = ["김소연", "윤하민", "이지원", "마얘지"]
+    
+    // MARK: - Life Cycle
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.navigationBar.isHidden = true
+        setNavigationBar(customNavigationBarView: customNavigationBarView, title: "", backBtnIsHidden: false, closeBtnIsHidden: false, bgColor: .grayBackground)
+        setStatusBar(.grayBackground)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        initUI()
+        setAction()
+        setTextField()
+        setCollectionView()
+    }
+}
+
+extension CreatePrivateDetailVC {
+    private func initUI() {
+        nextButton.isEnabled = false
+        nextButton.setTitleColor(.gray100, for: .normal)
+        nextButton.initViewBorder(borderWidth: 0, borderColor: UIColor.clear.cgColor, cornerRadius: 27, bounds: true)
+        
+        descriptionTextField.initTextFieldBorder(borderWidth: 1, borderColor: UIColor.grayStroke.cgColor, cornerRadius: 12, bounds: true)
+        descriptionTextField.setLeftPaddingPoints(15)
+        
+        memberCollectionView.isHidden = true
+        
+        addMemberButton.initViewBorder(borderWidth: 1, borderColor: UIColor.grayStroke.cgColor, cornerRadius: 10, bounds: true)
+    }
+    
+    private func setAction() {
+        nextButton.addAction(UIAction(handler: { _ in
+            guard let dvc = self.storyboard?.instantiateViewController(withIdentifier: "CompleteCreateVC") as? CompleteCreateVC else { return }
+            dvc.groupName = self.groupName
+            dvc.groupDescrption = self.descriptionTextField.text!
+            self.navigationController?.pushViewController(dvc, animated: true)
+        }), for: .touchUpInside)
+        
+        addMemberButton.addAction(UIAction(handler: { _ in
+            guard let dvc = self.storyboard?.instantiateViewController(withIdentifier: "AddMemberVC") else { return }
+            dvc.modalPresentationStyle = .fullScreen
+            self.present(dvc, animated: true, completion: nil)
+        }), for: .touchUpInside)
+    }
+    
+    private func setTextField() {
+        descriptionTextField.delegate = self
+    }
+    
+    private func setCollectionView() {
+        memberCollectionView.delegate = self
+        memberCollectionView.dataSource = self
+        
+        memberCollectionView.register(MemberCVC.self, forCellWithReuseIdentifier: MemberCVC.identifier)
+        
+        memberCollectionView.showsVerticalScrollIndicator = false
+    }
+}
+
+// MARK: - UICollectionView Delegate
+
+extension CreatePrivateDetailVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cellWidth = (collectionView.frame.width - 19 - 19) / 3
+        let cellHeight = cellWidth
+        return CGSize(width: cellWidth, height: cellHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 19
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 19
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .zero
+    }
+}
+
+extension CreatePrivateDetailVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return members.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MemberCVC.identifier, for: indexPath) as? MemberCVC else { return UICollectionViewCell() }
+        cell.initCell(name: members[indexPath.row])
+        return cell
+    }
+}
+
+
+// MARK: - UITextField Delegate
+
+extension CreatePrivateDetailVC: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        textField.becomeFirstResponder()
+        textField.initTextFieldBorder(borderWidth: 1, borderColor: UIColor.purple100.cgColor, cornerRadius: 12, bounds: true)
+        
+        descriptionCountLabel.textColor = .purple100
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField, reason: UITextField.DidEndEditingReason) {
+        textField.initTextFieldBorder(borderWidth: 1, borderColor: UIColor.grayStroke.cgColor, cornerRadius: 12, bounds: true)
+        
+        if descriptionTextField.hasText {
+            descriptionCountLabel.textColor = .black
+            
+            nextButton.isEnabled = true
+            nextButton.backgroundColor = .purple100
+            nextButton.setTitleColor(.white, for: .normal)
+        } else {
+            descriptionCountLabel.textColor = .grayTextNonInput
+            
+            nextButton.isEnabled = true
+            nextButton.backgroundColor = .grayTextNonInput
+            nextButton.setTitleColor(.gray100, for: .normal)
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        self.descriptionCountLabel.text = String("\(textField.text!.count)/15")
+    }
+}
