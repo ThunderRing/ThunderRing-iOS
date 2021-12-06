3e44f8b2fe20ebba5e3072c51c3e1a8ca171c36c
so yeon <alicesysy@swu.ac.kr>
2021-12-07 00:36:40 +0900

    [chore] 데이터모델 및 UI 수정

--- ThunderRing/ThunderRing/Sources/Lightning/VCs/LightningVC.swift
+++ ThunderRing/ThunderRing/Sources/Lightning/VCs/LightningVC.swift
@@ -15,6 +15,7 @@ class LightningVC: UIViewController {
     @IBOutlet weak var closeButton: UIButton!
     
     @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchBackView: UIView!
     @IBOutlet weak var groupListTableView: UITableView!
     
     // MARK: - Properties
@@ -42,8 +43,8 @@ class LightningVC: UIViewController {
 
 extension LightningVC {
     private func initUI() {
-        titleLabel.text = ""
-        view.backgroundColor = .background
+        titleLabel.text = "번개 치기"
+        searchBackView.backgroundColor = .background
     }
     
     private func setAction() {
@@ -55,15 +56,16 @@ extension LightningVC {
     private func setData() {
         privateGroup.append(contentsOf: [
             PrivateGroupDataModel(groupImage: "imgRabbit", groupName: "양파링걸즈", memberCounts: 4),
-            PrivateGroupDataModel(groupImage: "imgRabbit", groupName: "크롱", memberCounts: 30),
-            PrivateGroupDataModel(groupImage: "imgRabbit", groupName: "오렌지쥬스", memberCounts: 7)
+            PrivateGroupDataModel(groupImage: "imgCrong", groupName: "크롱", memberCounts: 30),
+            PrivateGroupDataModel(groupImage: "imgButterfly", groupName: "오렌지쥬스", memberCounts: 7),
+            PrivateGroupDataModel(groupImage: "imgJuju", groupName: "마법사쥬쥬", memberCounts: 7)
         ])
         
         publicGroup.append(contentsOf: [
-            PublicGroupDataModel(groupImage: "imgRabbit", groupName: "Rich ball", memberCounts: 3, hashTag: "사근한 오전"),
-            PublicGroupDataModel(groupImage: "imgRabbit", groupName: "곰돌아이", memberCounts: 7, hashTag: "북적이는 오후"),
-            PublicGroupDataModel(groupImage: "imgRabbit", groupName: "동물의 숲", memberCounts: 3, hashTag: "감성적인 새벽녘"),
-            PublicGroupDataModel(groupImage: "imgRabbit", groupName: "이지언니", memberCounts: 3, hashTag: "부지런한 동틀녘")
+            PublicGroupDataModel(groupImage: "imgRabbit", groupName: "Rich ball", memberCounts: 3, hashTag: "부지런한 동틀녘"),
+            PublicGroupDataModel(groupImage: "imgBear", groupName: "곰돌아이", memberCounts: 7, hashTag: "북적이는 오후"),
+            PublicGroupDataModel(groupImage: "imgNintendo", groupName: "동물의 숲", memberCounts: 3, hashTag: "감성적인 새벽녘"),
+            PublicGroupDataModel(groupImage: "imgDog", groupName: "이지언니", memberCounts: 3, hashTag: "사근한 오전")
         ])
     }
     
@@ -71,8 +73,9 @@ extension LightningVC {
         groupListTableView.delegate = self
         groupListTableView.dataSource = self
         
-        groupListTableView.separatorInset = UIEdgeInsets(top: 0, left: 25, bottom: 0, right: 25)
+        groupListTableView.separatorStyle = .none
         groupListTableView.backgroundColor = .background
+        groupListTableView.showsVerticalScrollIndicator = false
         
         groupListTableView.register(PrivateListTVC.self, forCellReuseIdentifier: PrivateListTVC.identifier)
         groupListTableView.register(PublicListTVC.self, forCellReuseIdentifier: PublicListTVC.identifier)
@@ -102,7 +105,7 @@ extension LightningVC: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         switch indexPath.section {
         case 0:
-            return 147
+            return 80
         case 1:
             return 96
         default:
@@ -121,6 +124,14 @@ extension LightningVC: UITableViewDelegate {
         }
         
         self.navigationController?.pushViewController(dvc, animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y > 10 {
+            searchBackView.layer.applyShadow()
+        } else {
+            searchBackView.layer.applyShadow(color: UIColor.clear, alpha: 0, x: 0, y: 0, blur: 0, spread: 0)
+        }
     }
 }
 
@@ -143,11 +154,33 @@ extension LightningVC: UITableViewDataSource {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: PrivateListTVC.identifier) as? PrivateListTVC else { return UITableViewCell() }
             cell.initCell(groupImage: privateGroup[indexPath.row].groupImage, groupName: privateGroup[indexPath.row].groupName, count: privateGroup[indexPath.row].memberCounts)
             cell.selectionStyle = .none
+            
+            if indexPath.row == 0 {
+                cell.clipsToBounds = true
+                cell.layer.cornerRadius = 9
+                cell.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            }
+            if indexPath.row == privateGroup.count - 1 {
+                cell.clipsToBounds = true
+                cell.layer.cornerRadius = 9
+                cell.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            }
             return cell
         case 1:
             guard let cell = tableView.dequeueReusableCell(withIdentifier: PublicListTVC.identifier) as? PublicListTVC else { return UITableViewCell() }
             cell.initCell(groupImage: publicGroup[indexPath.row].groupImage, groupName: publicGroup[indexPath.row].groupName, count: publicGroup[indexPath.row].memberCounts, hashTag: publicGroup[indexPath.row].hashTag)
             cell.selectionStyle = .none
+            
+            if indexPath.row == 0 {
+                cell.clipsToBounds = true
+                cell.layer.cornerRadius = 9
+                cell.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            }
+            if indexPath.row == privateGroup.count - 1 {
+                cell.clipsToBounds = true
+                cell.layer.cornerRadius = 9
+                cell.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+            }
             return cell
         default:
             return UITableViewCell()
