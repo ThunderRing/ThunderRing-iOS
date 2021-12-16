e99703ee844679d49d6a58d028443f15e48a1849
so yeon <alicesysy@swu.ac.kr>
2021-12-16 15:32:15 +0900

    [add] extension 추가

---
+++ ThunderRing/ThunderRing/Global/Extensions/UIImage+.swift
@@ -0,0 +1,20 @@
+//
+//  UIImage+.swift
+//  ThunderRing
+//
+//  Created by soyeon on 2021/12/15.
+//
+
+import UIKit
+
+extension UIImage {
+    func toPngString() -> String? {
+        let data = self.pngData()
+        return data?.base64EncodedString(options: .endLineWithLineFeed)
+    }
+  
+    func toJpegString(compressionQuality cq: CGFloat) -> String? {
+        let data = self.jpegData(compressionQuality: cq)
+        return data?.base64EncodedString(options: .endLineWithLineFeed)
+    }
+}
