29a6052f164c72f8aaaa5d37f8871c731595b0ae
so yeon <alicesysy@swu.ac.kr>
2022-02-09 18:22:57 +0900

    [Feat] Firebase DB에 번개 데이터 생성

--- ThunderRing/ThunderRing/Sources/Lightning/VCs/CompleteLightningVC.swift
+++ ThunderRing/ThunderRing/Sources/Lightning/VCs/CompleteLightningVC.swift
@@ -5,6 +5,8 @@
 //  Created by soyeon on 2021/11/24.
 //
 
+import FirebaseDatabase
+
 import UIKit
 
 class CompleteLightningVC: UIViewController {
@@ -26,6 +28,8 @@ class CompleteLightningVC: UIViewController {
     var minNumber: Int?
     var maxNumber: Int?
     
+    private let database = Database.database().reference()
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
@@ -43,13 +47,25 @@ extension CompleteLightningVC {
     
     private func setAction() {
         confirmButton.addAction(UIAction(handler: { _ in
+            // MARK: - TODO REMOVE
             lightningData.append(LightningDataModel(groupName: self.groupName!, lightningName: self.lightningName!, description: self.lightningDescription, date: self.date!, time: self.time!, location: self.location!, minNumber: self.minNumber!, maxNumber: self.maxNumber!))
             
             alarmData.append(AlarmDataModel(isThunder: false, isLightning: true, isFailed: false, lightningName: self.lightningName!, description: self.time! + " | " +  self.location!, time: "방금", groupName: self.groupName!))
             
-            self.dismiss(animated: true) {
-                dump(alarmData)
-            }
+            // Firebase Datebase로 데이터 저장
+            let lightningData: [String : Any] = [
+                "lightningName" : self.lightningName as Any,
+                "lightningDescription" : self.lightningDescription as Any,
+                "date" : self.date as Any,
+                "time" : self.time as Any,
+                "location" : self.location as Any,
+                "minNumber" : self.minNumber as Any,
+                "maxNumber" : self.maxNumber as Any
+            ]
+            guard let name = self.groupName else { return }
+            self.database.child(name).setValue(lightningData)
+
+            self.dismiss(animated: true)
             
             self.appDelegate?.scheduleNotification(groupName: self.groupName!, lightningName: self.lightningName!)
         }), for: .touchUpInside)
