32848f551c828a46ec2a9e965eaec5cd4242d5ab
so yeon <alicesysy@swu.ac.kr>
2022-02-27 02:25:09 +0900

    [Feat] 그룹 검색 UI 구현 (#71)

---
+++ ThunderRing/ThunderRing/Sources/PrivateGroup/VCs/MyPrivate/SearchPrivateGroupVC.swift
@@ -0,0 +1,39 @@
+//
+//  SearchPrivateGroupVC.swift
+//  ThunderRing
+//
+//  Created by soyeon on 2022/02/27.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SearchPrivateGroupVC: SearchBaseVC {
+
+    // MARK: - Properties
+    
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        setLayout()
+    }
+    
+    // MARK: - InitUI
+    
+    private func configUI() {
+        view.backgroundColor = .white
+        
+        navigationBarView.setTitle(title: "비공개 그룹")
+    }
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Custom Method
+}
