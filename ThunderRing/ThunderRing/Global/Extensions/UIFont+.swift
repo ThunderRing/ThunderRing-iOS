a57d1592c3402e94bd4a360d7473580c6f81593d
so yeon <alicesysy@swu.ac.kr>
2022-03-06 01:51:41 +0900

    [Del] 필요없는 Extension 삭제 (#64)

--- ThunderRing/ThunderRing/Global/Extensions/UIFont+.swift
+++ ThunderRing/ThunderRing/Global/Extensions/UIFont+.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-
 enum FontWeight {
     case bold, light, medium, regular, semiBold
 }
--- ThunderRing/ThunderRing/Global/Extensions/UIView+.swift
+++ ThunderRing/ThunderRing/Global/Extensions/UIView+.swift
@@ -26,28 +26,13 @@ extension UIView {
         self.layer.masksToBounds = bounds
     }
     
-    func addBottomBorderWithColor(color: UIColor) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: 0, y: self.frame.size.height, width: self.frame.size.width, height: 1)
-        self.layer.addSublayer(border)
-    }
-    
-    func addAboveTheBottomBorderWithColor(color: UIColor) {
-        let border = CALayer()
-        border.backgroundColor = color.cgColor
-        border.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
-        self.layer.addSublayer(border)
-    }
-    
     func makeRounded(cornerRadius : CGFloat?) {
         if let cornerRadius_ = cornerRadius {
             self.layer.cornerRadius = cornerRadius_
         }  else {
-            // cornerRadius 가 nil 일 경우의 default
+            /// cornerRadius 가 nil 일 경우의 default
             self.layer.cornerRadius = self.layer.frame.height / 2
         }
-        
         self.layer.masksToBounds = true
     }
 }
