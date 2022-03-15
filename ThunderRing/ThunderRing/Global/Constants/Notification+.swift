ca6bf8de4203acb5dc7044f7ae7e5c430853c198
so yeon <alicesysy@swu.ac.kr>
2022-03-15 12:45:14 +0900

    [Add] Extension 추가 (#81)

---
+++ ThunderRing/ThunderRing/Global/Constants/Notification+.swift
@@ -0,0 +1,14 @@
+//
+//  Notification+.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/03/11.
+//
+
+import Foundation
+
+extension Const {
+    struct Notification {
+        static let join = "JoinTheGroup"
+    }
+}
