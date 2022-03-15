3c009d8b2cbe6aa1301743fc793d9e295f236609
so yeon <alicesysy@swu.ac.kr>
2022-03-15 12:49:26 +0900

    [Add] 채팅뷰 추가 (#82)

---
+++ ThunderRing/ThunderRing/Sources/Chat/Views/ChatMainTopView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatMainTopView.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/03/15.
+//
+
+import UIKit
+
+class ChatMainTopView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
