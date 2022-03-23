1ef1c0348f9b9120fc8598934633058ce61aebf1
so yeon <alicesysy@swu.ac.kr>
2022-03-23 11:35:06 +0900

    [Fix] 공개그룹 더미데이터 수정 (#85)

--- ThunderRing/ThunderRing/Sources/Lightning/Models/PublicGroupDataModel.swift
+++ ThunderRing/ThunderRing/Sources/Lightning/Models/PublicGroupDataModel.swift
@@ -26,10 +26,10 @@ struct PublicGroupDataModel {
 }
 
 var publicGroupData : [PublicGroupDataModel] = [
-    PublicGroupDataModel(groupImage: "imgFlog", groupName: "서울숲 플로깅", memberCounts: 4, publicGroupType: .diligent, memberTotalCounts: 100, description: "같이 깨끗한 지구 만들어요"),
-    PublicGroupDataModel(groupImage: "imgFive", groupName: "05년생 모여", memberCounts: 7, publicGroupType: .crowd, memberTotalCounts: 10, description: "개그에 자신 있으신 분"),
-    PublicGroupDataModel(groupImage: "imgStudent", groupName: "서울중학교", memberCounts: 4, publicGroupType: .cozy, memberTotalCounts: 300, description: "서울중 동창 모여라"),
-    PublicGroupDataModel(groupImage: "imgGame", groupName: "닌텐도 할 사람", memberCounts: 3, publicGroupType: .soft, memberTotalCounts: 30, description: "고수만 들어오삼"),
-    PublicGroupDataModel(groupImage: "imgGame", groupName: "닌텐도 할 사람", memberCounts: 3, publicGroupType: .soft, memberTotalCounts: 30, description: "고수만 들어오삼")
+    PublicGroupDataModel(groupImage: "imgFlog", groupName: "숲 산책", memberCounts: 4, publicGroupType: .diligent, memberTotalCounts: 100, description: "같이 깨끗한 지구 만들어요"),
+    PublicGroupDataModel(groupImage: "imgFive", groupName: "05모임", memberCounts: 7, publicGroupType: .crowd, memberTotalCounts: 10, description: "개그에 자신 있으신 분"),
+    PublicGroupDataModel(groupImage: "imgStudent", groupName: "서울중", memberCounts: 4, publicGroupType: .cozy, memberTotalCounts: 300, description: "서울중 동창 모여라"),
+    PublicGroupDataModel(groupImage: "imgGame", groupName: "동숲하자", memberCounts: 3, publicGroupType: .soft, memberTotalCounts: 30, description: "고수만 들어오삼"),
+    PublicGroupDataModel(groupImage: "imgGame", groupName: "양파링걸", memberCounts: 3, publicGroupType: .soft, memberTotalCounts: 30, description: "고수만 들어오삼")
 ]
 
