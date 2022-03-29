ad5cad82df29f83888f2a3a7758ee9facc5824d6
so yeon <alicesysy@swu.ac.kr>
2022-03-30 05:58:12 +0900

    [Move] 비공개그룹 파일 Cells -> Views 하위로 이동, 그룹상세 상단 View 추가 (#68)

---
+++ ThunderRing/ThunderRing/Sources/PrivateGroup/Views/GroupDetailHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  GroupDetailHeaderView.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/03/30.
+//
+
+import UIKit
+
+class GroupDetailHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
