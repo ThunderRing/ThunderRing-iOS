8531cac14ae291db62a52dd80a620edf28016a0c
so yeon <alicesysy@swu.ac.kr>
2022-04-10 16:23:17 +0900

    [Fix] TDSNavigationBar 수정 (#110)

---
+++ ThunderRing/ThunderRing/Sources/Alarm/VCs/AlarmPopUpViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlarmPopUpViewController.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/04/10.
+//
+
+import UIKit
+
+class AlarmPopUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
