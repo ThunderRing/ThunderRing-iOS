ce9a23969a3d8a648f480e8528824a44a18180ec
so yeon <alicesysy@swu.ac.kr>
2022-04-10 16:24:08 +0900

    [Add] 알림 팝업 뷰 추가 (#110)

--- ThunderRing/ThunderRing/Sources/Alarm/VCs/AlarmPopUpViewController.swift
+++ ThunderRing/ThunderRing/Sources/Alarm/VCs/AlarmPopUpViewController.swift
@@ -7,23 +7,52 @@
 
 import UIKit
 
-class AlarmPopUpViewController: UIViewController {
+import SnapKit
+import Then
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+final class AlarmPopUpViewController: UIViewController {
 
-        // Do any additional setup after loading the view.
+    // MARK: - Properties
+    
+    private var popUpView = UIView().then {
+        $0.backgroundColor = .white
+    }
+
+    var alarmType: AlarmType = .lightning {
+        didSet {
+            switch alarmType {
+            case .thunder:
+                popUpView.backgroundColor = .purple100
+            case .lightning:
+                popUpView.backgroundColor = .yellow100
+            case .failed:
+                popUpView.backgroundColor = .red
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configUI()
+        setLayout()
     }
-    */
-
+    
+    // MARK: - Init UI
+    
+    private func configUI() {
+        view.backgroundColor = .init(red: 0, green: 0, blue: 0, alpha: 0.5)
+    }
+    
+    private func setLayout() {
+        view.addSubview(popUpView)
+        popUpView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(222)
+            $0.leading.trailing.equalToSuperview().inset(42)
+            $0.bottom.equalToSuperview().inset(242)
+        }
+    }
+    
+    // MARK: - Custom Method
 }
