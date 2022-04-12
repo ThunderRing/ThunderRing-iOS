76596ba88addfcb7c3fcbd2c068d1fa6f27d35de
so yeon <alicesysy@swu.ac.kr>
2022-04-13 02:34:44 +0900

    [Fix] 디자인시스템 QA 적용 (#112)

--- ThunderRing/ThunderRing/Global/DesignSystem/TDSButton.swift
+++ ThunderRing/ThunderRing/Global/DesignSystem/TDSButton.swift
@@ -47,7 +47,7 @@ final class TDSButton: UIButton {
     /// 디폴트 버튼 스타일 설정
     private func setDefaultStyle() {
         makeRounded(cornerRadius: 26.adjusted)
-        titleLabel?.font = .SpoqaHanSansNeo(type: .regular, size: 16)
+        titleLabel?.font = .SpoqaHanSansNeo(type: .medium, size: 16)
         backgroundColor = self.normalBgColor
         tintColor = .white
         setTitleColor(self.normalFontColor, for: .normal)
--- ThunderRing/ThunderRing/Global/DesignSystem/TDSTextField.swift
+++ ThunderRing/ThunderRing/Global/DesignSystem/TDSTextField.swift
@@ -41,8 +41,8 @@ class TDSTextField: UITextField {
         self.layer.borderColor = UIColor.gray300.cgColor
         self.layer.borderWidth = 1
         self.layer.cornerRadius = 10
-        self.setLeftPaddingPoints(15)
-        self.setRightPaddingPoints(15)
+        self.setLeftPaddingPoints(14)
+        self.setRightPaddingPoints(14)
     }
     
     // MARK: - Public Method
