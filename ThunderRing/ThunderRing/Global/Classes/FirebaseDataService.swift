ab7bf0c239aaf2f9a49288412c6f0366424696ea
Hamin <xhrrdl@HMs-MacBook-Air.local>
2022-04-16 22:07:29 +0900

    [feat] MyPage 데이터 연동

---
+++ ThunderRing/ThunderRing/Global/Classes/FirebaseDataService.swift
@@ -0,0 +1,34 @@
+//
+//  FirebaseDateService.swift
+//  ThunderRing
+//
+//  Created by Hamin on 2022/04/16.
+//
+
+import Foundation
+import Firebase
+
+//root
+fileprivate let baseRef = Database.database().reference()
+
+class FirebaseDataService {
+    
+    static let instance = FirebaseDataService()
+    
+    let userRef = baseRef.child("users")
+    
+    let groupRef = baseRef.child("groups")
+    
+    let messageRef = baseRef.child("message")
+    
+    //현재 접속중인 사용자의 uid
+    var currentUserUid: String? {
+        
+        get{
+            guard let uid = Auth.auth().currentUser?.uid else {return nil}
+            return uid
+        }
+        
+    }
+    
+}
