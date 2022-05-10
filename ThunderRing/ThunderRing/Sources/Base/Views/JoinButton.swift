dd7847dda1aaf68f863d1216a33d510bb62fb6e6
so yeon <alicesysy@swu.ac.kr>
2022-05-11 06:24:45 +0900

    [Feat] Alarm Base UI-버튼, 팝업 구현 (#126)

---
+++ ThunderRing/ThunderRing/Sources/Base/Views/JoinButton.swift
@@ -0,0 +1,20 @@
+//
+//  JoinButton.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/05/06.
+//
+
+import UIKit
+
+class JoinButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ThunderRing/ThunderRing/Sources/Base/Views/PopUpView.swift
@@ -0,0 +1,20 @@
+//
+//  PopUpView.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/05/06.
+//
+
+import UIKit
+
+class PopUpView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
