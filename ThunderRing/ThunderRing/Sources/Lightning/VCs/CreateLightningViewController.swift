82a2ed89dcf40f1f64645c147cdc23604a9c76d0
so yeon <alicesysy@swu.ac.kr>
2022-05-17 22:55:34 +0900

    [Add] 번개치기 ViewController 추가 (#138)

---
+++ ThunderRing/ThunderRing/Sources/Lightning/VCs/CreateLightningViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateLightningViewController.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/05/17.
+//
+
+import UIKit
+
+class CreateLightningViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
