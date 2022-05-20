e6d7029e82e720f72df025db4b695acffb5676bc
so yeon <alicesysy@swu.ac.kr>
2022-05-20 23:47:18 +0900

    [Del] 홈 메인 필요없는 파일 삭제 (#144)

---
+++ ThunderRing/ThunderRing/Global/Extensions/Int+.swift
@@ -0,0 +1,8 @@
+//
+//  Int+.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/05/19.
+//
+
+import Foundation
---
+++ ThunderRing/ThunderRing/Sources/Chat/Views/Chat/GuideView.swift
@@ -0,0 +1,20 @@
+//
+//  GuideView.swift
+//  ThunderRing
+//
+//  Created by 소연 on 2022/05/19.
+//
+
+import UIKit
+
+class GuideView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
